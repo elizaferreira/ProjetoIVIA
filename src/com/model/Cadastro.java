d97aeae3f66d7b97675abcba222199735a1b42a9
Elizabeth Ferreira <57023425+elizaferreira@users.noreply.github.com>
2019-10-28 16:25:50 -0300

    Update Cadastro.java

--- src/com/model/Cadastro.java
+++ src/com/model/Cadastro.java
@@ -11,15 +11,10 @@ public abstract class Cadastro {
 	private String nome;
 	private String ddd;
 	private Integer numero;
-	private String complemento;
-	private String municipio;
-	private String uf;
 	private String telefone1;
 	private String telefone2;
 	private String email;
-	private String fax;
-	private String logotipo;
-	private String contrato;
+	private String tipo;
 	
 	public String getNome() {
 		return nome;
@@ -45,24 +40,6 @@ public abstract class Cadastro {
 	public void setNumero(Integer numero) {
 		this.numero = numero;
 	}
-	public String getComplemento() {
-		return complemento;
-	}
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
-	public String getMunicipio() {
-		return municipio;
-	}
-	public void setMunicipio(String municipio) {
-		this.municipio = municipio;
-	}
-	public String getUf() {
-		return uf;
-	}
-	public void setUf(String uf) {
-		this.uf = uf;
-	}
 	public String getTelefone1() {
 		return telefone1;
 	}
@@ -75,23 +52,11 @@ public abstract class Cadastro {
 	public void setTelefone2(String telefone2) {
 		this.telefone2 = telefone2;
 	}
-	public String getFax() {
-		return fax;
+	public String getLogo() {
+		return tipo;
 	}
-	public void setFax(String fax) {
-		this.fax = fax;
-	}
-	public String getLogotipo() {
-		return logotipo;
-	}
-	public void setLogotipo(String logotipo) {
-		this.logotipo = logotipo;
-	}
-	public String getContrato() {
-		return contrato;
-	}
-	public void setContrato(String contrato) {
-		this.contrato = contrato;
+	public void settipo(String tipo) {
+		this.tipo = tipo;
 	}
 	public String getEmail() {
 		return email;
